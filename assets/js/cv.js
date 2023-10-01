a63e11ea2db44e8450843f21221923006d3d51b6
Lun Chok <146144812+tanlunchok@users.noreply.github.com>
2023-10-01 20:02:48 +1300

    remove local bootstrap css, add dark mode button

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -36,3 +36,23 @@ window.addEventListener('load', function() {
   progressBarSections.forEach(progressBarSection => {
     observer.observe(progressBarSection);
   }); */
+
+/*   const darkModeSwitch = document.getElementById('dark-mode-switch');
+
+  darkModeSwitch.addEventListener('click', function() {
+    darkModeSwitch.classList.toggle('dark-mode-switch-active');
+  
+    if (darkModeSwitch.classList.contains('dark-mode-switch-active')) {
+      darkModeSwitch.querySelector('i').classList.remove('fa-sun');
+      darkModeSwitch.querySelector('i').classList.add('fa-moon');
+    } else {
+      darkModeSwitch.querySelector('i').classList.remove('fa-moon');
+      darkModeSwitch.querySelector('i').classList.add('fa-sun');
+    }
+  }); */
+
+  const darkModeSwitch = document.getElementById('dark-mode-switch');
+
+darkModeSwitch.addEventListener('click', function() {
+  document.documentElement.setAttribute('data-bs-theme', darkModeSwitch.classList.contains('dark-mode-switch-active') ? 'dark' : 'light');
+});
