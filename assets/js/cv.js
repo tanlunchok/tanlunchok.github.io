15e346f3027dd6832d5210cc337d2df9e395e966
Lun Chok <146144812+tanlunchok@users.noreply.github.com>
2023-10-01 18:55:27 +1300

    progress bar animation

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -1,12 +1,36 @@
-function animateProgressBars() {
-    const progressBars = document.querySelectorAll('.progress-bar-loading');
+/* window.addEventListener('load', function() {
+    const progressBars = document.querySelectorAll('.progress');
   
     progressBars.forEach(progressBar => {
+      const percentage = progressBar.getAttribute('data-percentage');
+  
+      const progressBarElement = progressBar.querySelector('.progress-bar');
+  
+      progressBarElement.style.width = '0%';
+  
       anime({
-        targets: progressBar,
-        width: progressBar.getAttribute('data-percentage'),
-        duration: 0.5,
-        easing: 'easeInOutQuad',
+        targets: progressBarElement,
+        width: percentage,
+        duration: 500,
+        easing: 'easeInOutSine',
       });
     });
-  }
+  }); */
+  const progressBarSections = document.querySelectorAll('.lang-section');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      anime({
+        targets: entry.target.querySelector('.progress-bar'),
+        width: entry.target.getAttribute('data-percentage'),
+        duration: 500,
+        easing: 'easeInOutSine',
+      });
+    }
+  });
+});
+
+progressBarSections.forEach(progressBarSection => {
+  observer.observe(progressBarSection);
+});
