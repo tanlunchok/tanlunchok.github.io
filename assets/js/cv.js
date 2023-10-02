e1b3cbc04b84baa20b716d719ca831b349bc6469
Lun Chok <146144812+tanlunchok@users.noreply.github.com>
2023-10-03 10:32:06 +1300

    Update cv.js

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -3,7 +3,6 @@ window.addEventListener('load', function() {
   
     progressBars.forEach(progressBar => {
       const percentage = progressBar.getAttribute('data-percentage');
-  
       const progressBarElement = progressBar.querySelector('.progress-bar');
   
       progressBarElement.style.width = '0%';
@@ -18,26 +17,29 @@ window.addEventListener('load', function() {
 
 
   });
- 
+  window.onload = (event) => {
+    const accordionButton = document.querySelector('#edu-accordion-button');
 
-
-    
-/*     const accordionButton = document.querySelector('.accordion-button');
-    document.querySelectorAll('.accordion-button').forEach(accordionButton => {
-      accordionButton.addEventListener('click', function() {
-        if (this.classList.contains('collapsed')) {
-          this.classList.remove('collapsed');
-          this.setAttribute('aria-expanded', 'true');
-        } else {
-          this.classList.add('collapsed');
-          this.setAttribute('aria-expanded', 'false');
-        }
-      });
+    accordionButton.addEventListener('click', function() {
+      if (this.classList.contains('collapsed')) {
+        this.classList.remove('collapsed');
+        this.setAttribute('aria-expanded', 'true');
+      } else {
+        this.classList.add('collapsed');
+        this.setAttribute('aria-expanded', 'false');
+      }
     });
-
-    const darkModeSwitch = document.getElementById('dark-mode-switch');
+  };
   
+  
+  window.onload = (event) => {
+    const darkModeSwitch = document.getElementById('dark-mode-switch');
     darkModeSwitch.addEventListener('click', function() {
       document.documentElement.setAttribute('data-bs-theme', darkModeSwitch.classList.contains('dark-mode-switch-active') ? 'dark' : 'light');
     });
+  };
+    
+/*     
+
+    
    */
