330265383738a6b8c2e054906df4a9ca32f326a1
Lun Chok <146144812+tanlunchok@users.noreply.github.com>
2023-10-03 10:04:59 +1300

    fix js function

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -1,28 +1,6 @@
-window.addEventListener('load', function() {
-    const progressBars = document.querySelectorAll('.progress');
-  
-    progressBars.forEach(progressBar => {
-      const percentage = progressBar.getAttribute('data-percentage');
-  
-      const progressBarElement = progressBar.querySelector('.progress-bar');
-  
-      progressBarElement.style.width = '0%';
-  
-      anime({
-        targets: progressBarElement,
-        width: percentage,
-        duration: 500,
-        easing: 'easeInOutSine',
-      });
-    });
-
-
-  });
- 
-
-  document.ready(function() {
+document.ready(function() {
     
-    const accordionButton = document.querySelector('.accordion-button');
+/*     const accordionButton = document.querySelector('.accordion-button');
     document.querySelectorAll('.accordion-button').forEach(accordionButton => {
       accordionButton.addEventListener('click', function() {
         if (this.classList.contains('collapsed')) {
@@ -40,7 +18,28 @@ window.addEventListener('load', function() {
     darkModeSwitch.addEventListener('click', function() {
       document.documentElement.setAttribute('data-bs-theme', darkModeSwitch.classList.contains('dark-mode-switch-active') ? 'dark' : 'light');
     });
-  
+   */
   
   
   });
+
+window.addEventListener('load', function() {
+  const progressBars = document.querySelectorAll('.progress');
+
+  progressBars.forEach(progressBar => {
+    const percentage = progressBar.getAttribute('data-percentage');
+
+    const progressBarElement = progressBar.querySelector('.progress-bar');
+
+    progressBarElement.style.width = '0%';
+
+    anime({
+      targets: progressBarElement,
+      width: percentage,
+      duration: 500,
+      easing: 'easeInOutSine',
+    });
+  });
+
+
+});
