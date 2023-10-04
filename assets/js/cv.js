f4918430651aee85a8f661a94aa099dde798d01f
Lun Chok <146144812+tanlunchok@users.noreply.github.com>
2023-10-04 16:24:57 +1300

    Update cv.js

--- assets/js/cv.js
+++ assets/js/cv.js
@@ -24,7 +24,6 @@ window.addEventListener('load', function() {
     const darkModeSwitch = document.getElementById('dark-mode-switch');
     const buttonIcon = document.getElementById('dark-mode-switch-icon');
     const initialThemeCheck = document.documentElement.getAttribute('data-bs-theme');
-    const dataIconAttribute = darkModeSwitch.getAttribute('data-icon');
 
     if (initialThemeCheck === 'dark') {
       darkModeSwitch.classList.add('dark-mode');
@@ -40,12 +39,12 @@ window.addEventListener('load', function() {
         document.documentElement.setAttribute('data-bs-theme', 'dark');
         darkModeSwitch.classList.remove('light-mode');
         darkModeSwitch.classList.add('dark-mode');
-        dataIconAttribute.setAttribute('data-icon', 'moon');
+        buttonIcon.setAttribute('data-icon', 'moon');
       } else {
         document.documentElement.setAttribute('data-bs-theme', 'light');
         darkModeSwitch.classList.remove('dark-mode');
         darkModeSwitch.classList.add('light-mode');
-        dataIconAttribute.setAttribute('data-icon', 'sun');
+        buttonIcon.setAttribute('data-icon', 'sun');
       }
     });
   };
